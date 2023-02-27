56465aacc8a1add5ca0ea4baa267984cc73d43e0
angel <angelcruz.feb@gmail.com>
2023-02-27 14:07:15 -0600

    first commit

---
+++ licores/liquors/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from dateutil.relativedelta import relativedelta
+
+class Liquor(models.Model):
+    destilado = models.CharField(max_length=50,default='alcohol',blank=True) 
+    nombre = models.CharField (max_length=50,default='???',blank=True) 
+    description = models.TextField(default='Licor',blank=True)
+    pais_origen= models.CharField (max_length=50,default='Mexico',blank=True)
+    size = models.CharField (max_length=50,default='1L',blank=True)
+    tipo_envase = models.CharField (max_length=50,default='Botella de cristal',blank=True)
+    fecha_ingreso = models.DateField (default=timezone.now)
+    caducidad = models.DateField (default= timezone.datetime.today() + relativedelta(months=+3))
+    edicion = models.CharField (max_length=50,default='Standar')
+    precio = models.FloatField (default=1000)
+
+
