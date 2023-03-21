ffad4245d53dba529d7239d5f4b5f5e2dee76a04
angel <angelcruz.feb@gmail.com>
2023-03-21 14:19:51 -0600

    update release

--- licores/liquors/tests.py
+++ licores/liquors/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from graphene_django.utils.testing import GraphQLTestCase
+from mixer.backend.django import mixer
+import graphene
+import json
