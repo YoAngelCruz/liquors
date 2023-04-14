3cae9a050f6bd3ebb7ca6c6f7c9eecb3ab9d216e
angel <angelcruz.feb@gmail.com>
2023-04-14 14:11:02 -0600

    create unit test

--- licores/liquors/tests.py
+++ licores/liquors/tests.py
@@ -18,6 +18,13 @@ LIQUORS_QUERY = '''
   }
 }
 '''
+CREATE_LIQUORS_MUTATION = '''
+ mutation createLiquorMutation($destilado: String,$nombre: String,$description: String,$paisOrigen: String,$size: String,$tipoEnvase: String,$edicion: String,$precio: Float) {
+     createLiquor(destilado: $destilado,nombre: $nombre,description: $description,paisOrigen: $paisOrigen,size: $size,tipoEnvase: $tipoEnvase,edicion: $edicion,precio: $precio) {
+        nombre
+     }
+ }
+'''
 
 class LiquorTestCase(GraphQLTestCase):
     GRAPHQL_SCHEMA = schema
@@ -37,3 +44,16 @@ class LiquorTestCase(GraphQLTestCase):
         print("query liquors results ")
         print(content)
         assert len(content['data']['liquors']) == 2
+
+    def test_createLiquor_mutation(self):
+
+        response = self.query(
+            CREATE_LIQUORS_MUTATION,
+            variables={'destilado':'Tequila','nombre':'Sinsimito','description':'Tequila del rubio','paisOrigen':'Mexico','size':'750ml','tipoEnvase':'cristal','edicion':'premium','precio':250000.0}
+        )
+        print('mutation ')
+        print(response)
+        content = json.loads(response.content)
+        print(content)
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual({"createLiquor": {"nombre":"Sinsimito"}}, content['data'])
